32e6001837cdc798042eb101c1fffdae7f83642b
Priyanshu <priyanshu.xyz.085@gmail.com>
2026-05-27 15:16:25 +0530

    feat: Add database migration files for users, accounts, sessions, and verifications with initial schema setup.

---
+++ server/src/modules/database/supabase/migrations/0000_marvelous_spirit.sql
@@ -0,0 +1,8 @@
+CREATE TABLE "users" (
+	"id" uuid PRIMARY KEY NOT NULL,
+	"email" varchar(255) NOT NULL,
+	"password" varchar(255) NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	"updated_at" timestamp with time zone,
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
