43a0225236410c89f4368cfe7e1a77ad8ffd40e2
Priyanshu <work.priyanshu085@gmail.com>
2026-06-19 19:09:15 +0530

    feat: pre-commit

--- server/src/lib/tigerbeetle/account.service.ts
+++ server/src/lib/tigerbeetle/account.service.ts
@@ -13,7 +13,7 @@ function hashUserId(userId: string): bigint {
 
 const userIdReverseMap = new Map<bigint, string>();
 
-export class AccountService implements IAccountRepository {
+export class TigerBeetleAccountService implements IAccountRepository {
   private readonly client = TBClient;
   private readonly accountRepository: IAccountRepository = new AccountRepository();
 
